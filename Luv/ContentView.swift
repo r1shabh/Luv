449ab6a40598726d2571764b1e9b112082be930f
Tyler Flesch <ftyler20@vt.edu>
2019-11-19 14:54:53 -0500

    Test

--- Luv/ContentView.swift
+++ Luv/ContentView.swift
@@ -12,25 +12,16 @@ struct ContentView: View {
     @State private var selection = 0
  
     var body: some View {
-        TabView(selection: $selection){
-            Text("First View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("first")
-                        Text("First")
-                    }
-                }
-                .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
-                }
-                .tag(1)
+        TabView {
+            Top10().tabItem {
+                Text("Top10")
+            }
+            Locations().tabItem {
+                Text("Locations")
+            }
+            Ideas().tabItem {
+                Text("Ideas")
+            }
         }
     }
 }
